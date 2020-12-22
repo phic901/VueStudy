01cbde9f6b32c9ba53ce307ec80be56fbaf6afe4
Seunghyeon Lee <Seunghyeon Lee@DESKTOP-L87PFF0>
2020-12-22 23:05:51 +0900

    ..

---
+++ 20201222-todolist-Basic/src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+<v-container>
+  <v-layout row wrap>
+    <v-flex xs6 pa-2>
+      <List
+        :todoList="todoList"
+        @statusControl="statusControl"
+        @listDelete="listDelete"
+      />
+    </v-flex>
+    <v-flex xs6 pa-2>
+      <ListAdd @listAdd="listAdd"
+      />
+    </v-flex>
+  </v-layout>
+
+</v-container>
+</template>
+
+
+
+<script>
+import List from './List'
+import ListAdd from './ListAdd'
+export default {
+  components : {
+    List,
+    ListAdd
+  },
+  data(){
+    return {
+      todoList : []
+    }
+  },
+  methods : {
+    listAdd(memo){
+      console.log('받았어!');
+      this.todoList.push(
+        {memo : memo,
+        status: 'created'}
+        );
+    },
+    statusControl(index, status){
+      this.todoList[index].status = status;
+    },
+    listDelete(index){
+      //todoList에서 index번째 리스트를 찾은후 그 요소부터 시작해서 1개를 지워줘
+      this.todoList.splice(index, 1);
+    }
+  }
+}
+</script>
---
+++ 20201222-todolist-Basic/src/components/List.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <v-card
+            class="pa-3"
+            :class="{'done' : list.status === 'done'}"
+            v-for="(list, index) in todoList"
+            :key="index"
+        >
+            <p>{{list.memo}}</p>
+            <p>{{list.status}}</p>
+            <v-btn 
+                v-if="list.status === 'created'"
+                @click="$emit('statusControl', index, 'done')"
+                fab flat small color="green"
+            >
+                <i class="fas fa-check"></i>
+            </v-btn>
+
+            <v-btn fab flat small color="blue"
+                v-else
+                @click="$emit('statusControl', index, 'created')"
+            >
+                <i class="fas fa-redo-alt"></i>
+            </v-btn>
+
+
+            <v-btn fab flat small color="red"
+                @click="$emit('listDelete', index)"
+            >
+                <i class="fas fa-trash-alt"></i>
+            </v-btn>
+
+
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    props : ['todoList']
+}
+</script>
+
+<style scoped>
+.done{background-color: rgba(0, 0, 0, 0.1);}
+.done p{text-decoration: line-through;color: rgba(0, 0, 0, 0.5);}
+</style>
