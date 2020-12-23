4ce1c7393df54a69a2a3ecbaa12767f6a6501440
Seunghyeon Lee <Seunghyeon Lee@DESKTOP-L87PFF0>
2020-12-24 02:26:13 +0900

    ..

--- 20200124-vuex-project-for-share/src/store.js
+++ 20200124-vuex-project-for-share/src/store.js
@@ -5,6 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    allUsers:[
+      {userId: 'hoza123', password: '123', name: 'Hoza', address: 'Seoul', src:'https://goo.gl/oqLfJR'},
+      {userId: 'max123', password: '456', name: 'Max', address: 'Berlin', src:'https://goo.gl/Ksk9B9'},
+      {userId: 'lego123', password: '789', name: 'Lego', address: 'Busan', src:'https://goo.gl/x7SpCD'}
+    ]
+  },
+  //computed 랑 비슷함
+  getters : {
+    //function(state){ 같이 스테이트를 쓰곘다고 알려줘야함
+    allUsersCount : function(state){
+      return state.allUsers.length;
+    }
+    /**
+     * allUsersCount : state => {
+     *  return state.allUsers.length; 위랑 같음
+     * }
+     */
 
   },
   mutations: {
